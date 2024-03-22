bb53be90e04a54af03d55e120a2c2a546d3b9705
L <88348716+MadamHippo@users.noreply.github.com>
2024-03-22 07:59:48 -0700

    Create Diff21.js

---
+++ Diff21.js
@@ -0,0 +1,14 @@
+function diff21(n){
+  
+  let diff = Math.abs(n-21);
+  
+  if (n <= 21){
+    return diff
+    
+  } else {
+    return diff*2
+  }
+
+}
+
+// https://the-winter.github.io/codingjs/exercise.html?name=diff21&title=Warmup-1
