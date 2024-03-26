0cd177c70e4afb4726d0aa1115ea9a35bf950f5f
L <88348716+MadamHippo@users.noreply.github.com>
2024-03-25 22:55:49 -0700

    Create Smilingmonkey.js

---
+++ Smilingmonkey.js
@@ -0,0 +1,9 @@
+function monkeyTrouble(aSmile, bSmile){
+  
+  if (aSmile === bSmile) {
+    return true
+  } 
+  return false
+}
+
+// https://the-winter.github.io/codingjs/exercise.html?name=monkeyTrouble&title=Warmup-1
